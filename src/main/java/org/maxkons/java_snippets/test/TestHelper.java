d6c68579dbcf89dd399a781eac821b8072bd3a18
Max <konstantinov.maxim@gmail.com>
2016-12-01 22:23:49 +0300

    change callPrivateMethod design

--- src/main/java/org/maxkons/java_snippets/test/TestHelper.java
+++ src/main/java/org/maxkons/java_snippets/test/TestHelper.java
@@ -5,27 +5,33 @@ import org.junit.Test;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static junit.framework.Assert.assertEquals;
 
 public class TestHelper {
 
+    @SuppressWarnings("unchecked")
     @Test
     public void testInnerClass() {
         InnerClass systemUnderTest = new InnerClass();
-        assertEquals(4, callPrivateMethod(systemUnderTest, "addNumbersInternal", 2, 2));
+        List<Integer> in = Arrays.asList(1, 2);
+        List<Integer> out = (List<Integer>) callPrivateMethod(systemUnderTest, "addNumbersInternal", new Class[]{List.class, int.class}, new Object[]{in, 1});
+        assertEquals(
+                Arrays.asList(2, 3),
+                out
+        );
     }
 
     /**
      * Call a private method, this method can't work with primitive data type, because of boxing.
      */
-    public static Object callPrivateMethod(Object objectToUse, String methodToCall, Object... methodArgs) {
-        @SuppressWarnings("RedundantCast")
-        Class[] argClasses = (Class[]) Arrays.stream(methodArgs).map(Object::getClass).toArray(Class[]::new);
+    public static Object callPrivateMethod(Object objectToUse, String methodToCall, Class[] argsTypes, Object[] args) {
         try {
-            Method method = objectToUse.getClass().getDeclaredMethod(methodToCall, argClasses);
+            Method method = objectToUse.getClass().getDeclaredMethod(methodToCall, argsTypes);
             method.setAccessible(true);
-            return method.invoke(objectToUse, methodArgs);
+            return method.invoke(objectToUse, args);
         } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
             throw new RuntimeException(e);
         }
@@ -36,12 +42,8 @@ public class TestHelper {
 
 class InnerClass {
 
-    public Integer addNumbers(Integer a, Integer b) {
-        return addNumbersInternal(a, b);
-    }
-
-    private Integer addNumbersInternal(Integer a, Integer b) {
-        return a + b + 1; // Wrong logic testing should discover
+    private List<Integer> addNumbersInternal(List<Integer> list, int b) {
+        return list.stream().map(a -> a + b + 1).collect(Collectors.toList()); // Wrong logic that should be discovered in a test
     }
 
 }
